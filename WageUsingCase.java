d7c44788178952375e45a859e9501c0d7d94f8c4
nayab jahan <nayabzafir@gmail.com>
2022-06-27 20:13:09 +0530

    Added : daily wage using swicth

---
+++ WageUsingCase.java
@@ -0,0 +1,28 @@
+public class WageUsingCase {
+    //Constants variacles
+   public static final int Is_Part_time = 1;
+    public static final int Is_Full_time = 2;
+    public static final int Employ_Rate_Per_hr = 20 ;
+
+    public static void main(String[] args){
+        // Variables
+    int employ_Hr = 0;
+    int employ_wage = 0;
+    //Computation
+        int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch (empcheck) {
+            case Is_Part_time:
+                employ_Hr = 4;
+                break;
+            case Is_Full_time:
+                employ_Hr = 8;
+                break;
+            default:
+                employ_Hr = 0;
+        }
+        employ_wage = Employ_Rate_Per_hr * employ_Hr;
+        System.out.println("daily wage of employ is "+ employ_wage);
+
+        }
+}
+
